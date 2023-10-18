eb028064243894bb0fb37a2226a3e96f48a79f49
Chuei-Zuo Chen <louisa0103a@gmail.com>
2023-10-18 17:20:00 +0800

    clean

--- detect.py
+++ detect.py
@@ -1,42 +1,56 @@
+# Load openCV library
 import cv2
 
+# Use OpenCV FaceRecognizer.
 recognizer = cv2.face.LBPHFaceRecognizer_create()
+# Load the model that we trained before.
 recognizer.read('face.yml')
+# Load face detector model, to fetch all the faces points and head rectangle.
 cascade_path = "xml/haarcascade_frontalface_default.xml"
+# Use face detector model to mapping the OpenCV video streaming frame to recognise which face is belone to which person.
 face_cascade = cv2.CascadeClassifier(cascade_path)
 
+# Start OpenCV video streaming.
 cap = cv2.VideoCapture(0)
+# Check the camera is opened.
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
 while True:
-    ret, img = cap.read()
-    if not ret:
+    # Read stream frames and isolate images and rectangles.
+    rectangle, image = cap.read()
+    if not rectangle:
         print("Cannot receive frame")
         break
-    img = cv2.resize(img, (540, 300))
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray)
-
+    # Image resize.
+    image = cv2.resize(image, (540, 300))
+    # Turn images from RGB to Gray level.
+    input_gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Fetch face points
+    faces = face_cascade.detectMultiScale(input_gray_image)
+    # Initial ID enum.
     name = {
         '1': 'Potti',
         '2': 'Didi',
         '3': 'Eric',
-        '4': 'Someone Else'
     }
-
+    # Looping faces also put labels and draw rectangle on each faces.
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        idnum, confidence = recognizer.predict(gray[y:y + h, x:x + w])
-        # print(confidence)
-        if confidence < 65:
-            print(name[str(idnum)])
-            text = name[str(idnum)]
+        # draw rectangle on each frame's image.
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # Use our model that just trained as the recognizer to predict each face.
+        id_number, confidence_level = recognizer.predict(input_gray_image[y:y + h, x:x + w])
+        # After predict we will also get the confidence, this one is to identify likelihood for each face.
+        print(confidence_level)
+        if confidence_level < 65:
+            # If confidence level reach our target, then we will put the name by id enum on the view.
+            print(name[str(id_number)])
+            text = name[str(id_number)]
         else:
-            text = '???'
-        cv2.putText(img, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            text = ''
+        cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
-    cv2.imshow('default', img)
+    cv2.imshow('default', image)
     if cv2.waitKey(5) == ord('q'):
         break
 cap.release()
--- main.py
+++ main.py
@@ -2,39 +2,61 @@ import cv2
 import numpy as np
 
 if __name__ == '__main__':
+    # Load face detector model, to fetch all the faces points and head rectangle.
     detector = cv2.CascadeClassifier('xml/haarcascade_frontalface_default.xml')
     recog = cv2.face.LBPHFaceRecognizer_create()
-    faces = []
-    ids = []
+    face_detect_result_collection = []
+    face_id = []
 
+    # Load face01 images(Potti's image dataset)
     for i in range(1, 29):
+        # Read image
         img = cv2.imread(f'face01/{i}.jpg')
+        # Turn image from RBG to Gray level
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Save image into unit8 data format
         img_np = np.array(gray, 'uint8')
+        # Using open cv CascadeClassifier to detect image, it will return all the face points location and the head rectangle.
         face = detector.detectMultiScale(gray)
         for (x, y, w, h) in face:
-            faces.append(img_np[y:y + h, x:x + w])
-            ids.append(1)
-
-    for i in range(1, 17):
+            # Collect face data
+            face_detect_result_collection.append(img_np[y:y + h, x:x + w])
+            # Temp list for store the face ID
+            face_id.append(1)
+    # Load face02 images(Didi's image dataset)
+    for i in range(1, 30):
+        # Read image
         img = cv2.imread(f'face02/{i}.jpg')
+        # Turn image from RBG to Gray level
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Save image into unit8 data format
         img_np = np.array(gray, 'uint8')
+        # Using open cv CascadeClassifier to detect image, it will return all the face points location and the head rectangle.
         face = detector.detectMultiScale(gray)
         for (x, y, w, h) in face:
-            faces.append(img_np[y:y + h, x:x + w])
-            ids.append(2)
-
+            # Collect face data
+            face_detect_result_collection.append(img_np[y:y + h, x:x + w])
+            # Temp list for store the face ID
+            face_id.append(2)
+    # Load face03 images(Eric's image dataset)
     for i in range(1, 31):
-        img = cv2.imread(f'face02/{i}.jpg')
+        # Read image
+        img = cv2.imread(f'face03/{i}.jpg')
+        # Turn image from RBG to Gray level
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Save image into unit8 data format
         img_np = np.array(gray, 'uint8')
+        # Using open cv CascadeClassifier to detect image, it will return all the face points location and the head rectangle.
         face = detector.detectMultiScale(gray)
         for (x, y, w, h) in face:
-            faces.append(img_np[y:y + h, x:x + w])
-            ids.append(3)
+            # Collect face data
+            face_detect_result_collection.append(img_np[y:y + h, x:x + w])
+            # Temp list for store the face ID
+            face_id.append(3)
 
     print('training model...')
-    recog.train(faces, np.array(ids))
+    # Here is the ML training entry point, feed face detection's result collections and face id to it for training ML model.
+    recog.train(face_detect_result_collection, np.array(face_id))
+    # Save new model that we just trained.
     recog.save('face.yml')
     print('ok!')
